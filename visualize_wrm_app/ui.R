67cdb2c8324230ea7634ee94c7d2cc373ae8bd92
streckereck <colin.ferster@gmail.com>
2022-07-11 11:16:53 -0700

    drop header

--- visualize_wrm_app/ui.R
+++ visualize_wrm_app/ui.R
@@ -15,19 +15,7 @@ ui <- fluidPage(
   # help from: https://stackoverflow.com/questions/60151766/adding-images-in-the-title-and-at-the-right-of-navbarpage
   
   tags$head(
-    includeHTML(("google-analytics.html")),
-    tags$link(rel = "shortcut icon", type = "image/png", href = "wrm_logo.png"),
-    tags$title("Walk Roll Map Visualize")
-  ),
-  navbarPage(
-    tags$script(HTML("var header = $('.navbar > .container-fluid');
-header.append('<div style=\"float:right\"><a href=\"https://walkrollmap.org/\"><img src=\"wrm_logo.png\" alt=\"Walk Roll Map Logo\" style=\"float:right;width:35px;height:41px;padding-top:5px;padding-left:5px;\"> </a></div>');
-    console.log(header)")
-    ),
-    tags$script(HTML("var header = $('.navbar > .container-fluid');
- header.append('<div style=\"float:right\"><h4>data from <a href=\"https://walkrollmap.org/\">walkrollmap.org </h4></a></div>');"
-    )),
-    title = "Walk Roll Map Visualize"
+    includeHTML(("google-analytics.html"))
   ),
 
   sidebarLayout(
